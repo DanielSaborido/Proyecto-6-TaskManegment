85c1407821052089c92edcb4951e6a7967af0c3d
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-02-03 20:01:34 +0100

    refactorizacion del login

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,40 +1,81 @@
 <template>
-    <h1>{{title}}</h1>
-    <form>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            v-model=email
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            v-model=password
-          />
-        </div>
-        <a href="">Forget password?</a>
-        <button type="submit">Log in</button>
-      </form>
-      <button >Create Account</button>
+    <form v-show=login>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          v-model=email
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          v-model=password
+        />
+      </div>
+      <a @click="forget=true; login=false">Forget password?</a>
+      <button type="submit">Log in</button>
+    </form>
+    <button v-show=login @click="create=true; login=false">Create Account</button>
+    <form v-show=create>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          v-model=email
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          v-model=password
+        />
+      </div>
+      <div>
+        <label htmlFor="Rpassword">Repeat password:</label>
+        <input
+          type="Rpassword"
+          id="Rpassword"
+          name="Rpassword"
+          v-model=Rpassword
+        />
+      </div>
+      <button type="submit" @click="create=false; login=true">Create Account</button>
+    </form>
+    <form v-show=forget>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          v-model=email
+        />
+      </div>
+      <button type="submit" @click="forget=false; login=true">Sent recover</button>
+    </form>
   </template>
   
   <script>
-    export default {
-      props:{
-          title: String,
-      },
-  
+    export default {  
       data() {
           return{
             email: "",
             password: "",
+            Rpassword: "",
+            login: true,
+            create: false,
+            forget: false,
           }
       },
   
@@ -42,6 +83,9 @@
         validateEmail() {
             const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
             return re.test(this.email)
+        },
+        validatePassword() {
+            return this.password === this.Rpassword
         }
       },
   
--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <router-link to="/">Home</router-link >
+        <h1>{{ title }}</h1>
         <router-link to="/login">Login</router-link>
     </div>
 </template>
