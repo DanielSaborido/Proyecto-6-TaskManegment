176dd6dbc02e4f61aedb5512beaae532f67ad520
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-02-05 21:00:58 +0100

    navbar finalizado

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,27 +1,56 @@
 <template>
   <nav>
     <router-link to="/" class="icon">
-      <img src="../../assets/dark/home.png" alt="Home" class="icon">
-    </router-link >
-    <h1>{{ Title }}</h1>
-    <user>
-      <img src="../../assets/dark/sol-y-luna.png" alt="darck/light" class="icon">
+      <img v-if="theme === 'light'" src="../../assets/light/home.png" alt="Home" class="icon">
+      <img v-else src="../../assets/dark/home.png" alt="Home" class="icon">
+    </router-link>
+    <h1>{{ title }}</h1>
+    <div>
+      <img v-if="theme === 'light'" src="../../assets/light/sol-y-luna.png" alt="dark/light" class="icon" @click="changeTheme">
+      <img v-else src="../../assets/dark/sol-y-luna.png" alt="dark/light" class="icon" @click="changeTheme">
       <router-link to="/contact" class="icon">
-        <img src="../../assets/dark/contact-icon.png" alt="Contact" class="icon">
+        <img v-if="theme === 'light'" src="../../assets/light/contact-icon.png" alt="Contact" class="icon">
+        <img v-else src="../../assets/dark/contact-icon.png" alt="Contact" class="icon">
       </router-link>
       <router-link to="/login" class="icon">
-        <img src="../../assets/dark/user.png" alt="user" class="icon">
+        <img v-if="theme === 'light'" src="../../assets/light/user.png" alt="User" class="icon">
+        <img v-else src="../../assets/dark/user.png" alt="User" class="icon">
       </router-link>
-    </user>
+    </div>
   </nav>
 </template>
 
 <script>
   export default {
     computed: {
-      Title() {
+      title() {
         return this.$route.meta.title || 'Default Title'
       },
+    },
+    data() {
+      return {
+        theme: "",
+      }
+    },
+    methods: {
+      loadTheme() {
+        this.theme = localStorage.getItem('theme')
+      },
+      changeTheme(){
+        console.log(this.theme)
+        this.theme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark'
+        console.log(this.theme)
+        localStorage.setItem('theme', this.theme)
+        document.documentElement.setAttribute('data-theme', this.theme)
+      },
+      // getThemeImagePath(imageName) {
+      //   const folder = this.theme === 'light' ? 'light' : 'dark'
+      //   console.log(`../../assets/${folder}/${imageName}.png`)
+      //   return `../../assets/${folder}/${imageName}.png`
+      // },
+    },
+    created() {
+      this.loadTheme()
     },
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import router from "./components/router/router.js"
 import './style.css'
 import App from './App.vue'
 
+const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+localStorage.setItem('theme', theme)
+document.documentElement.setAttribute('data-theme', theme)
+
 createApp(App).use(router).mount('#app')
